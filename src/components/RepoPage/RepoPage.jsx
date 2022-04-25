a80b4b78d36a39c774913d4a65a323046d3c1848
PGRISEWOOD <49351886+PGRISEWOOD@users.noreply.github.com>
2022-04-25 13:28:00 +0100

    add mui, initial layout

--- src/components/RepoPage/RepoPage.jsx
+++ src/components/RepoPage/RepoPage.jsx
@@ -1,5 +1,17 @@
 import React, { useState } from "react";
 import { debounce } from "lodash";
+import {
+  Grid,
+  TextField,
+  Box,
+  Card,
+  InputLabel,
+  FormControl,
+  Select,
+  MenuItem,
+  Button,
+  CircularProgress,
+} from "@mui/material";
 import { useRepos } from "../../hooks/useRepos/useRepos";
 
 const LANGUAGES = [
@@ -23,38 +35,74 @@ const RepoPage = () => {
   const { items, total_count: totalCount } = repos || {};
 
   const handleSearch = debounce(({ target: { value } }) => {
-    console.log(value);
     setSearch(value);
   }, 1000);
 
   const handleLanguageChange = ({ target: { value } }) => {
-    console.log({ value });
     setLanguage(value);
   };
 
+  const handleLoadMore = () => {};
+
   return (
-    <div className="repo-page">
-      <input className="repo-page__search-input" onChange={handleSearch} />
-      <select onChange={handleLanguageChange}>
-        {LANGUAGES.map((lang) => (
-          <option value={lang} key={lang}>
-            {lang}
-          </option>
-        ))}
-      </select>
-      {!loading && (
-        <div className="repo-page__repo-list">
-          <h2>{`Showing ${1}-${
-            items?.length
-          } of ${totalCount} repositories`}</h2>
-          {items?.map(({ id, name, stargazers_count: stars }) => (
-            <ul key={id}>
-              {name}----{stars}
-            </ul>
-          ))}
-        </div>
-      )}
-    </div>
+    <Box>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            id="outlined-basic"
+            label="Search repositories"
+            variant="outlined"
+            onChange={handleSearch}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Language</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={language}
+              label="Language"
+              onChange={handleLanguageChange}
+            >
+              {LANGUAGES.map((lang) => (
+                <MenuItem value={lang} key={lang}>
+                  {lang}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        {items && (
+          <>
+            <Grid item xs={12}>
+              <h2>{`Showing ${1}-${
+                items?.length
+              } of ${totalCount} repositories`}</h2>
+            </Grid>
+            {items?.map(({ id, name, stargazers_count: stars }) => (
+              <Grid item key={id} xs={12} md={6} lg={3}>
+                <Card variant="outlined">
+                  {name}----{stars}
+                </Card>
+              </Grid>
+            ))}
+          </>
+        )}
+        {loading && (
+          <Grid item xs={12}>
+            <CircularProgress />
+          </Grid>
+        )}
+        {items && !loading && (
+          <Grid item xs={12}>
+            <Button variant="contained" onClick={handleLoadMore}>
+              Load more
+            </Button>
+          </Grid>
+        )}
+      </Grid>
+    </Box>
   );
 };
 
